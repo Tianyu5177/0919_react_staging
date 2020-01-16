5ae2c3081cafbe37f3612cacc4a4baaa35ecd2ef
tianyu5177 <zhangtianyu5177@sina.com>
2020-01-16 16:02:26 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'
+import Add from './components/add/add'
+import List from './components/list/list'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+
+	state = {
+		comments:[]
+	}
+
+	//此方法用于添加一个评论，需要接收一个评论对象
+	addComment = (commentObj)=>{
+		//1.获取原状态
+		let {comments} = this.state
+		comments.unshift(commentObj)
+		this.setState({comments})
+	}
+
+	//此方法用于删除一个评论，需要接收一个评论的id
+	deleteComment = (id)=>{
+		let {comments} = this.state
+		comments = comments.filter((commentObj)=>{
+			return commentObj.id !== id
+		})
+		this.setState({comments})
+	}
+
+	render(){
+		const {comments} = this.state
+		return(
+			<div id="app">
+				<div>
+					<header className="site-header jumbotron">
+						<div className="container">
+							<div className="row">
+								<div className="col-xs-12">
+									<h1>请发表对React的评论</h1>
+								</div>
+							</div>
+						</div>
+					</header>
+					<div className="container">
+						<Add addComment={this.addComment}/>
+						<List comments={comments} deleteComment={this.deleteComment}/>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
-
-export default App;
