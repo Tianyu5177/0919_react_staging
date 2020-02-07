a0bb9d5bbcad35dba43832d979750d37da745ca4
tianyu5177 <zhangtianyu5177@sina.com>
2020-02-07 16:27:03 +0800

    更新

--- 05_github搜索案例/App.js
+++ 05_github搜索案例/App.js
@@ -3,14 +3,27 @@ import Search from './components/search/search'
 import List from './components/list/list'
 
 export default class App extends Component {
+
+	state = {
+		isFirst:true,//标识是否第一次打开
+		isLoading:false,//标识是否加载中
+		error:'',//错误信息
+		users:[]//所有用户信息
+	}
+
+	//更新App的状态
+	updateAppState = (obj)=>{
+		this.setState(obj)
+	}
+
 	render() {
 		return (
 			<div className="container">
 				<section className="jumbotron">
 					<h3 className="jumbotron-heading">Search Github Users</h3>
-					<Search/>
+					<Search updateAppState={this.updateAppState}/>
 				</section>
-				<List/>
+				<List {...this.state}/>
 			</div>
 		)
 	}
--- 05_github搜索案例/components/search/search.jsx
+++ 05_github搜索案例/components/search/search.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import PubSub from 'pubsub-js'
 
 export default class Search extends Component {
 
@@ -8,24 +7,25 @@ export default class Search extends Component {
 		//1.获取用户的输入
 		const {value} = this.refs.keyWord
 		//2.发送请求之前，展示loading状态
-		PubSub.publish('atguigu',{isFirst:false,isLoading:true});
+		this.props.updateAppState({isFirst:false,isLoading:true})
 		//3.发送请求
 		axios.get('https://api.github.com/search/users',{params:{q:value}}).then(
 			(response)=>{
 				//更新状态
-				PubSub.publish('atguigu',{
+				this.props.updateAppState({
 					isLoading:false,
 					users:response.data.items
-				});
+				})
 			},
 			(err)=>{
 				//更新状态
-				PubSub.publish('atguigu',{
+				this.props.updateAppState({
 					isLoading:false,
 					error:err.message
-				});
+				})
 			}
 		)
+		//3.根据返回的数据，更新状态
 	}
 
 	render() {
