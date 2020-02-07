d922b9e23e9f42a952253b11e3f3cec88b40129d
tianyu5177 <zhangtianyu5177@sina.com>
2020-02-07 17:21:20 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react'
-import Search from './components/search/search'
-import List from './components/list/list'
+import Home from './components/home/home'
+import About from './components/about/about'
+import {Link,Route} from 'react-router-dom'
 
 export default class App extends Component {
 	render() {
 		return (
-			<div className="container">
-				<section className="jumbotron">
-					<h3 className="jumbotron-heading">Search Github Users</h3>
-					<Search/>
-				</section>
-				<List/>
+			<div>
+				<div className="row">
+					<div className="col-xs-offset-2 col-xs-8">
+						<div className="page-header"><h2>React Router Demo</h2></div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-2 col-xs-offset-2">
+						<div className="list-group">
+							<Link className="list-group-item" to="/about">About</Link>
+							<Link className="list-group-item" to="/home">Home</Link>
+						</div>
+					</div>
+					<div className="col-xs-6">
+						<div className="panel">
+							<div className="panel-body">
+								<Route path="/about" component={About}/>
+								<Route path="/home" component={Home}/>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
