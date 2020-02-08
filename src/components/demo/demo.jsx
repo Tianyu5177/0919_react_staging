fb12dbbbfe39a5a3531040ad55757a0186faa012
tianyu5177 <zhangtianyu5177@sina.com>
2020-02-08 14:55:57 +0800

    更新

---
+++ src/components/demo/demo.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Demo extends Component {
+	render() {
+		//console.log(this);
+		return (
+			<h3>
+				我是Demo组件的内容！
+			</h3>
+		)
+	}
+}
